74d8c90b7fe4a5ad0f294ff9092436701d1b737b
grissharrisdennis <117310678+grissharrisdennis@users.noreply.github.com>
2024-08-27 06:43:26 +0530

    Add files via upload

---
+++ frontend/src/router/index.js
@@ -0,0 +1,44 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import RegisterPage from '../views/RegisterPage.vue'
+import LoginPage from '../views/LoginPage.vue'
+import UserPage from '../views/UserPage.vue'
+import UserHistory from '../views/HistoryPage.vue'
+
+const routes = [
+  {
+    path: '',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path:'/register',
+    name:'RegisterPage',
+    component:RegisterPage
+  },
+  {
+    path:'/login',
+    name:'UserLogin',
+    component:LoginPage
+  },
+  {
+    path:'/:id/user/',
+    name:'UserPage',
+    component:UserPage
+  },
+  {
+    path:'/:id/user/history',
+    name:'UserHistory',
+    component:UserHistory,
+    props:true
+  }
+  
+]
+
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
