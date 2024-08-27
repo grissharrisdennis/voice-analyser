47dc05fbebf38151a7ef118a788fe24f53649679
grissharrisdennis <117310678+grissharrisdennis@users.noreply.github.com>
2024-08-27 14:09:15 +0530

    Add files via upload

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """
 ASGI config for backend project.
 
@@ -15,21 +14,3 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_asgi_application()
-=======
-"""
-ASGI config for backend project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
-"""
-
-import os
-
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-
-application = get_asgi_application()
->>>>>>> a09a68ea8dfed890562f7050b1354fefe75e7213
