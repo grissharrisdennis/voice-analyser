aba298622c69fc9f09b270b4f6adcf41efd44e8e
grissharrisdennis <117310678+grissharrisdennis@users.noreply.github.com>
2024-08-27 13:41:57 +0530

    Update tests.py

--- backend/backendapp/tests.py
+++ backend/backendapp/tests.py
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
+
 from django.test import TestCase
 
 # Create your tests here.
-=======
-from django.test import TestCase
 
-# Create your tests here.
->>>>>>> a09a68ea8dfed890562f7050b1354fefe75e7213
