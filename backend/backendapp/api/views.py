98e3b9c5b4f3cc3bdf7e8c4fc7b3de2b174a982a
grissharrisdennis <117310678+grissharrisdennis@users.noreply.github.com>
2024-08-28 22:30:30 +0530

    Update views.py

--- backend/backendapp/api/views.py
+++ backend/backendapp/api/views.py
@@ -2,7 +2,7 @@ from rest_framework.response import Response
 from rest_framework import status, viewsets, generics
 from django.shortcuts import render,get_object_or_404
 from rest_framework.permissions import IsAuthenticated, AllowAny
-from ..models import AudioFile, Transcription,Transcription, WordFrequency
+from ..models import AudioFile, Transcription,WordFrequency
 from .serializers import AudioFileSerializer, TranscriptionSerializer, AudioFileDetailSerializer,UserModelSerializer,WordFrequencySerializer
 from pydub import AudioSegment
 from django.contrib.auth.models import User
