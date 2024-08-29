d0e0179f7d19bcb84c27a062e72b33087cd9c2b5
grissharrisdennis <117310678+grissharrisdennis@users.noreply.github.com>
2024-08-29 09:50:31 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-a#7(v0lu4_6#xll23j4sp$#cpv!2^1tbo+l=6zv^pneuz8aqv7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['GrissHarrisDennis.pythonanywhere.com']
+ALLOWED_HOSTS = ['grissharrisdennis.pythonanywhere.com']
 
 
 
@@ -46,8 +46,8 @@ INSTALLED_APPS = [
 ]
 
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS=['https://voice-analyser-xi.vercel.app/']
-CSRF_TRUSTED_ORIGINS = ['https://voice-analyser-xi.vercel.app/']
+CORS_ALLOWED_ORIGINS=['https://voice-analyser-xi.vercel.app']
+CSRF_TRUSTED_ORIGINS = ['https://voice-analyser-xi.vercel.app']
 # Enable HTTPS cookies
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
