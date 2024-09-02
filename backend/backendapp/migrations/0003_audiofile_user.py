503331a9df4aec4231d64be2e2ef3a2454d9103f
grissharrisdennis <117310678+grissharrisdennis@users.noreply.github.com>
2024-09-02 15:46:55 +0530

    Update 0003_audiofile_user.py

--- backend/backendapp/migrations/0003_audiofile_user.py
+++ backend/backendapp/migrations/0003_audiofile_user.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('backendapp', '0002_remove_transcription_language'),
+        ('backendapp', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
